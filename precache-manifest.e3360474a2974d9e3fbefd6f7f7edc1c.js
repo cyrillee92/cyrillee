dbd1b112938b60c313df7ef0f952aa800c89d06a
cyrillee92 <42854470+cyrillee92@users.noreply.github.com>
2019-06-03 17:11:41 +0800

    init

---
+++ precache-manifest.e3360474a2974d9e3fbefd6f7f7edc1c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "26333b3884782fdb5f8c0a971f1d2570",
+    "url": "fonts/materialdesignicons-webfont.26333b38.woff"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "af3f6dbe172a9c1aba77366a2ff630d1",
+    "url": "fonts/materialdesignicons-webfont.af3f6dbe.woff2"
+  },
+  {
+    "revision": "f9c639613fb4fe59a318",
+    "url": "js/chunk-2d22d746.28d51327.js"
+  },
+  {
+    "revision": "c4b89f359e82c2a55555",
+    "url": "js/chunk-c5b6b254.ed761419.js"
+  },
+  {
+    "revision": "29588cc5dc6e46b76019",
+    "url": "js/chunk-vendors.1819b48e.js"
+  },
+  {
+    "revision": "6d57993de49040485d3a",
+    "url": "js/app.dc179720.js"
+  },
+  {
+    "revision": "377c7d0a408e1f5b22f42a799eaf1e65",
+    "url": "index.html"
+  },
+  {
+    "revision": "032eb679a327d4668d7537c32c314fbc",
+    "url": "img/materialdesignicons-webfont.032eb679.svg"
+  },
+  {
+    "revision": "baded94134f273450a473a4962111324",
+    "url": "fonts/materialdesignicons-webfont.baded941.ttf"
+  },
+  {
+    "revision": "1993c4a16cce5446a5cfafacf4da740b",
+    "url": "fonts/materialdesignicons-webfont.1993c4a1.eot"
+  },
+  {
+    "revision": "29588cc5dc6e46b76019",
+    "url": "css/chunk-vendors.5653fcaf.css"
+  },
+  {
+    "revision": "6d57993de49040485d3a",
+    "url": "css/app.7d3b1dcc.css"
+  }
+];
